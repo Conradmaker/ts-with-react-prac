6dc0a14915402041c400131110dc7045e44ab66b
conrad <yhg0337@naver.com>
2020-12-29 23:50:28 +0900

    Initialize settings

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+module.exports = {
+  mode: 'development', //production
+  devtool: 'eval', //hidden-source-map
+  resolve: {
+    extensions: ['.jsx', '.js', '.tsx', 'ts'],
+  },
+  entry: {
+    app: './src/client.tsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+      },
+    ],
+  },
+  plugins: [],
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist'),
+  },
+};
